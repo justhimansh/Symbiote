b7be099bf8877b82e2ac0d4edd1059a6eba1e5aa
Lobou2203 <131317554+Lobou2203@users.noreply.github.com>
2023-09-15 20:18:01 +1200

    Sound Effects
    Adding the initial sound effects function

--- MERN-Symbiote/src/Components/Home.jsx
+++ MERN-Symbiote/src/Components/Home.jsx
@@ -2,14 +2,18 @@ import { Link } from 'react-router-dom';
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import sound1 from '../SoundEffects/sound1.mp3';
+import sound2 from '../SoundEffects/sound2.mp3';
+import sound3 from '../SoundEffects/sound3.mp3';
 
 function Landing() {
   var AWS = require("aws-sdk");
-  AWS.config.accessKeyId = '' ;
-  AWS.config.secretAccessKey = '' ;
+ 
+  AWS.config.accessKeyId = 'AKIAU4MU2XJ4GYTB5P3W';
+  AWS.config.secretAccessKey = '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';
   AWS.config.region = 'us-west-2';
 
-  const API_KEY = '';
+  const API_KEY = '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';
 
   const { transcript, listening, resetTranscript } = useSpeechRecognition();
 
@@ -22,7 +26,6 @@ function Landing() {
       "Content-Type": "application/json",
       Authorization: `Bearer ${API_KEY}`,
     };
-
     const data = {
       model: "gpt-3.5-turbo",
       messages: [
@@ -30,7 +33,6 @@ function Landing() {
         { role: "user", content: transcript },
       ],
     };
-
     try {
       const response = await axios.post("https://api.openai.com/v1/chat/completions", data, { headers });
       const generatedText = response.data.choices[0].message.content;
@@ -55,7 +57,6 @@ function Landing() {
 
   const talk = (text) => {
     const polly = new AWS.Polly();
-
     const params = {
       OutputFormat: "mp3",
       Text: text,
@@ -74,7 +75,6 @@ function Landing() {
         if (audioRef.current) {
           audioRef.current.src = URL.createObjectURL(blob);
           audioRef.current.play();
-
           audioRef.current.addEventListener('ended', () => {
             SpeechRecognition.startListening();
           });
@@ -83,6 +83,35 @@ function Landing() {
     });
   };
 
+  function SoundEffectButton() {
+    const effects = [sound1, sound2, sound3];
+    const soundRef = useRef(null);
+
+    const playRandomEffect = async () => {
+      if (!soundRef.current) return;
+      const randomEffectIndex = Math.floor(Math.random() * effects.length);
+      const chosenEffect = effects[randomEffectIndex];
+      soundRef.current.src = chosenEffect;
+
+      soundRef.current.load();
+
+      soundRef.current.oncanplaythrough = () => {
+        soundRef.current.play();
+      }
+
+      soundRef.current.onerror = (e) => {
+        console.error("Error playing audio:", e);
+      }
+    }
+
+    return (
+      <div>
+        <button onClick={playRandomEffect}>Sounds</button>
+        <audio ref={soundRef} />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Welcome to Symbiote</h1>
@@ -90,6 +119,7 @@ function Landing() {
       {listening && <p>Listening...</p>}
       <p>Content goes here: {responseText}</p>
       <audio ref={audioRef} controls />
+      <SoundEffectButton />
     </div>
   );
 }
