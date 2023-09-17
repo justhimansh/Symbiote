299e3a4bb46a7892410f0828100fa543f4e98694
Johnny Set <ztk7656@autuni.ac.nz>
2023-09-17 22:36:05 +1200

    Changed the GIF
    Changed the GIF to more of a Goo-like substance

--- MERN-Symbiote/src/Components/Home.jsx
+++ MERN-Symbiote/src/Components/Home.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect, useState, useRef } from 'react';
-import axios from 'axios';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import sound1 from '../Files/sound1.mp3';
-import sound2 from '../Files/sound2.mp3';
-import sound3 from '../Files/sound3.mp3';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import sound1 from "../Files/sound1.mp3";
+import sound2 from "../Files/sound2.mp3";
+import sound3 from "../Files/sound3.mp3";
 //import V_paper from '../Files/V_paper.jpg';
-import Venom from '../Files/Venom.gif';
-import VenomVoice from '../Files/VenomVoice.mp3';
-
+import Venom from "../Files/symbiote_goo.gif";
+import VenomVoice from "../Files/VenomVoice.mp3";
 
 function Landing() {
   var AWS = require("aws-sdk");
-  AWS.config.accessKeyId = 'AKIAU4MU2XJ4GYTB5P3W';
-  AWS.config.secretAccessKey = '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';
-  AWS.config.region = 'us-west-2';
+  AWS.config.accessKeyId = "AKIAU4MU2XJ4GYTB5P3W";
+  AWS.config.secretAccessKey = "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";
+  AWS.config.region = "us-west-2";
 
-  const API_KEY = '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';
+  const API_KEY = "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";
 
   const { transcript, listening, resetTranscript } = useSpeechRecognition();
-  const [responseText, setResponseText] = useState('');
+  const [responseText, setResponseText] = useState("");
   const audioRef = useRef(null);
 
   const GPT = async () => {
@@ -29,12 +30,20 @@ function Landing() {
     const data = {
       model: "gpt-3.5-turbo",
       messages: [
-        { role: "system", content: "Pretend you're a personal assistant. Limit your responses to 2 sentences MAX." },
+        {
+          role: "system",
+          content:
+            "Pretend you're a personal assistant. Limit your responses to 2 sentences MAX.",
+        },
         { role: "user", content: transcript },
       ],
     };
     try {
-      const response = await axios.post("https://api.openai.com/v1/chat/completions", data, { headers });
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        data,
+        { headers }
+      );
       const generatedText = response.data.choices[0].message.content;
       setResponseText(generatedText);
       talk(generatedText);
@@ -75,7 +84,7 @@ function Landing() {
           audioRef.current.play().catch((error) => {
             console.warn("Playback was not triggered by a user action.", error);
           });
-          audioRef.current.addEventListener('ended', () => {
+          audioRef.current.addEventListener("ended", () => {
             SpeechRecognition.startListening();
           });
         }
@@ -95,13 +104,16 @@ function Landing() {
       soundRef.current.load();
       soundRef.current.oncanplaythrough = () => {
         soundRef.current.play().catch((error) => {
-          console.warn("Sound effect playback was not triggered by a user action.", error);
+          console.warn(
+            "Sound effect playback was not triggered by a user action.",
+            error
+          );
         });
-      }
+      };
       soundRef.current.onerror = (e) => {
         console.error("Error playing audio:", e);
-      }
-    }
+      };
+    };
 
     return (
       <div>
@@ -112,28 +124,30 @@ function Landing() {
   }
 
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: "center" }}>
       <h1>Welcome to Symbiote</h1>
-  
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        border: '1px solid black',
-        width: '300px',
-        height: '300px',
-        margin: '0 auto',
-        position: 'relative',
-        backgroundImage: `url(${Venom})`,
-        backgroundSize: '200%',
-        backgroundPosition: 'center center',
-        backgroundRepeat: 'no-repeat'
-      }}>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          border: "1px solid black",
+          width: "450px",
+          height: "400px",
+          margin: "0 auto",
+          position: "relative",
+          backgroundImage: `url(${Venom})`,
+          backgroundSize: "200%",
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <button
           style={{
-            position: 'absolute',
-            bottom: '10px',
-            right: '10px'
+            position: "absolute",
+            bottom: "10px",
+            right: "10px",
           }}
           onClick={() => SpeechRecognition.startListening()}
         >
@@ -141,21 +155,20 @@ function Landing() {
         </button>
         <div
           style={{
-            position: 'absolute',
-            bottom: '10px',
-            left: '10px'
+            position: "absolute",
+            bottom: "10px",
+            left: "10px",
           }}
         >
           <SoundEffectButton />
         </div>
       </div>
-  
+
       {listening && <p>Listening...</p>}
       <p>Content goes here: {responseText}</p>
       <audio ref={audioRef} controls />
     </div>
   );
-  
 }
 
 export default Landing;
